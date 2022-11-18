177774064d84ea45463a4efb51b69c4b41361489
DESKTOP-IASUES3\abe <barbozabraham@gmail.com>
2022-11-18 10:36:29 -0800

    Added project dependencies

---
+++ ers/src/main/java/com/revature/App.java
@@ -0,0 +1,19 @@
+package com.revature;
+import io.javalin.Javalin;
+
+
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        
+        
+        Javalin app = Javalin.create();
+        app.get("/", ctx -> ctx.result("Hello World"))
+            .start(7070);
+        
+    }
+}
---
+++ ers/src/test/java/com/revature/project1/AppTest.java
@@ -0,0 +1,27 @@
+package com.revature.project1;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+
+    //public static Calculator calc = new Calculator();
+
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+
+       
+
+        assertTrue( true );
+    }
+}
